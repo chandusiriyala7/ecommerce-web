f46b411117b17bc43ff5c3cded2be1eda860ee2a
chandusiriyala <chandusiriyala116@gmail.com>
2025-02-19 01:26:00 +0530

    User details Storing in Redux and Fetching on Refresh

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,8 +1,19 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from './userSlice'
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './userSlice';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // Uses localStorage
+
+const persistConfig = {
+  key: 'user',
+  storage,
+};
+
+const persistedUserReducer = persistReducer(persistConfig, userReducer);
 
 export const store = configureStore({
   reducer: {
-    user : userReducer
+    user: persistedUserReducer,
   },
-})
+});
+
+export const persistor = persistStore(store);
